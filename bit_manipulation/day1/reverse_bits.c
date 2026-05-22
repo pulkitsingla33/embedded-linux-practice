593411886763ad1aaa7cdfd668163dc49e1b2195
Pulkit Singla <pulkit8590@gmail.com>
2026-05-22 21:54:39 +0530

    Corrected bit reversal code

--- bit_manipulation/day1/reverse_bits.c
+++ bit_manipulation/day1/reverse_bits.c
@@ -7,7 +7,7 @@ uint32_t reverse_bits(uint32_t num)
     num = ((num & 0xFF00FF00) >> 8) | ((num & 0xFF00FF) << 8);
     num = ((num & 0xF0F0F0F0) >> 4) | ((num & 0xF0F0F0F) << 4);
     num = ((num & 0xCCCCCCCC) >> 2) | ((num & 0x33333333) << 2);
-    num = ((num & 0xAAAAAAAA) >> 1) | ((num & 0x55555555) << 2);
+    num = ((num & 0xAAAAAAAA) >> 1) | ((num & 0x55555555) << 1);
 
     return num;
 }
