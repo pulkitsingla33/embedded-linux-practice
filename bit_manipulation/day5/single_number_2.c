29353be95a3e153da94222efef17b8f3ee3d5b26
Pulkit Singla <pulkit8590@gmail.com>
2026-05-27 23:54:48 +0530

    Bit Manipulation Day 5

---
+++ bit_manipulation/day5/single_number_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr[] = {1, 1, 1, 2, 2, 2, 3,3,3, 40};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int ones = 0, twos = 0, threes = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        twos |= ones & arr[i];
+        ones ^= arr[i];
+        threes = ones & twos;
+        ones &= ~threes;
+        twos &= ~threes;
+    }
+
+    printf("The single number is: %d\n", ones);
+    return 0;
+}
